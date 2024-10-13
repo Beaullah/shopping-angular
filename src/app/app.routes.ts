ee609127abd7b030a1f1a4c5fd3a0aef5adfae6c
Esther Fummey <bonita19fummey@gmail.com>
2024-10-13 12:58:46 +0000

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { BodyComponent } from './body/body.component';
+import { ProductsComponent } from './products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'body',
+    component: BodyComponent,
+  },
+  {
+    path: 'products',
+    component: ProductsComponent,
+  },
+];
